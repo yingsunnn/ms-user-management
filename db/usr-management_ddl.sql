c7d0fa012f53f87d8a67018fcfdae9964ca12fe4
Ying Sun <ying.s@kelptech.org>
2022-02-05 22:34:22 -0500

    added db

---
+++ db/usr-management_ddl.sql
@@ -0,0 +1,66 @@
+create table user (
+    id BINARY(16) null,
+    full_name varchar(64) null,
+    email varchar(128) null,
+    gender varchar(16) null,
+    birthday bigint(16) null,
+    created_at bigint(16) not null,
+    created_by bigint(16) null,
+    updated_at bigint(16) not null,
+    constraint user_pk
+        primary key (id)
+);
+
+create table user_credential
+(
+    id int AUTO_INCREMENT,
+    user_id binary(16) not null,
+    credential_type varchar(16) not null,
+    password varchar(256) not null,
+    salt varchar(128) not null,
+    status varchar(16) not null,
+    created_at bigint(16) not null,
+    updated_at bigint(16) not null,
+    constraint user_credential_pk
+        primary key (id)
+);
+
+create table permission
+(
+    id int auto_increment,
+    permission_name varchar(64) not null,
+    description varchar(512) null,
+    constraint permission_pk
+        primary key (id)
+);
+
+create table role
+(
+    id int auto_increment,
+    role_name varchar(128) not null,
+    description varchar(512) null,
+    status varchar(16) not null,
+    created_at bigint(16) not null,
+    updated_at bigint(16) not null,
+    created_by int null,
+    constraint role_pk
+        primary key (id)
+);
+
+create table role_permission
+(
+    id int auto_increment,
+    role_id int not null,
+    permission_id int not null,
+    constraint role_permission_pk
+        primary key (id)
+);
+
+create table user_role
+(
+    id int auto_increment,
+    user_id int not null,
+    role_id int not null,
+    constraint user_role_pk
+        primary key (id)
+);
