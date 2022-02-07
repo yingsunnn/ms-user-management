f767e6bf0a47a5abde4f5ff948b1aea0a406b958
Ying Sun <ying.s@kelptech.org>
2022-02-07 12:22:31 -0500

    Added check duplicated credential endpoint

--- src/main/java/com/ying/msusermanagement/controller/UserController.java
+++ src/main/java/com/ying/msusermanagement/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ying.msusermanagement.controller;
 
 import com.ying.msusermanagement.Permissions;
+import com.ying.msusermanagement.dto.UserCredentialDto;
 import com.ying.msusermanagement.dto.UserDto;
 import com.ying.msusermanagement.service.UserService;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +34,24 @@ public class UserController {
       @RequestBody UserDto user
   ) {
     return this.userService.createUser(user);
+  }
+
+  @GetMapping(value = "/credentials", produces = "application/json")
+  public String getDuplicatedUserCredentials (
+      @RequestParam("credentialType") String credentialType,
+      @RequestParam("credentialId") String credentialId) {
+
+    int count = this.userService.checkCredentialExists(
+        UserCredentialDto.builder()
+            .credentialType(credentialType)
+            .credentialId(credentialId)
+            .build()
+    );
+
+    if (count > 0) {
+      return "{ \"credentialExist\": true }";
+    } else {
+      return "{ \"credentialExist\": false }";
+    }
   }
 }
